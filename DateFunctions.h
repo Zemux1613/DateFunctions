5ca08813f07afa6a1183720ae3dcdd0023a620ab
Zemux1613 <42244022+Zemux1613@users.noreply.github.com>
2021-12-18 11:33:26 +0100

    feat: add test for the isLeapYear method

--- DateFunctions.h
+++ DateFunctions.h
@@ -1,1 +1,8 @@
 #pragma once
+struct Date {
+    int day;
+    int month;
+    int year;
+};
+
+bool isLeapYear(Date* date);
--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,22 @@
+#include "DateFunctions.h";
+#include <stdio.h>
+
 int main() {
+
+    Date* date = new Date();
+
+    date->day = 12;
+    date->month = 3;
+    date->year = 4567;
+
+    if (isLeapYear(date)) {
+        printf("%d.%d.%d is a leap year!\n", date->day, date->month, date->year);
+    }
+    else {
+        printf("%d.%d.%d isn't a leap year!\n", date->day, date->month, date->year);
+    }
+
+    delete date;
 
     return 0;
 }
