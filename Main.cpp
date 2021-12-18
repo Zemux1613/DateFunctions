83b3ac884059e538bf013df1a396e662b5a03194
Zemux1613 <42244022+Zemux1613@users.noreply.github.com>
2021-12-18 12:01:52 +0100

    feat: add test for isEarlierThan

--- Main.cpp
+++ Main.cpp
@@ -4,6 +4,7 @@
 int main() {
 
     Date* date1 = inputDate();
+    Date* date2 = inputDate();
 
     if (isValidDate(date1)) {
 
@@ -19,7 +20,23 @@ int main() {
         printf("%d.%d.%d isn't a a valid gregoria date!\n", date1->day, date1->month, date1->year);
     }
 
+    if (isValidDate(date1) && isValidDate(date2)) {
+
+        if(isEarlierThan(date1, date2)){
+
+            printf("%d.%d.%d is earlier than %d.%d.%d", date1->day, date1->month, date1->year, date2->day, date2->month, date2->year);
+
+        }
+        else {
+
+            printf("%d.%d.%d isn't earlier than %d.%d.%d", date1->day, date1->month, date1->year, date2->day, date2->month, date2->year);
+
+        }
+
+    }
+
     delete date1;
+    delete date2;
 
     return 0;
 }
